d6830815104f0248d3ad870830c29c9fd6a1e9a3
r-lyeh <35402248+r-lyeh@users.noreply.github.com>
2024-11-01 20:25:59 +0100

    flatten path lib

--- api.h
+++ api.h
@@ -7,7 +7,7 @@
 #include "inc/cimport.h"
 #include "inc/scene.h"
 #include "inc/postprocess.h"
-#pragma comment(lib, "ext/ext-assimp/x64/assimp")
+#pragma comment(lib, "assimp")
 
 #ifdef __cplusplus
 extern "C" {
